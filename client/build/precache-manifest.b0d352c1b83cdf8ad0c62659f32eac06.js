008a1a656cb59f3dbcaa75cb07eeec7828cf4003
Matt Hughes <zeepkdb@gmail.com>
2020-09-18 22:24:52 -0700

    build

---
+++ client/build/precache-manifest.b0d352c1b83cdf8ad0c62659f32eac06.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c9278559da16ea4c4ebd8be8f26dcf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9731e49dc86c44f4d206",
+    "url": "/static/css/2.7d0f1a9f.chunk.css"
+  },
+  {
+    "revision": "d0b2dd4a80658d32a987",
+    "url": "/static/css/main.bac0e9e8.chunk.css"
+  },
+  {
+    "revision": "9731e49dc86c44f4d206",
+    "url": "/static/js/2.c54d8839.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c54d8839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0b2dd4a80658d32a987",
+    "url": "/static/js/main.90e3d761.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "5794a35e76f1b130fbfbdec955779ded",
+    "url": "/static/media/logo.5794a35e.png"
+  },
+  {
+    "revision": "159263bf4976843830365d52d1f94fba",
+    "url": "/static/media/primeicons.159263bf.eot"
+  },
+  {
+    "revision": "2e3a6b48408add8bb604d0b4e820a3b5",
+    "url": "/static/media/primeicons.2e3a6b48.ttf"
+  },
+  {
+    "revision": "4e915114c908962a6ff2d505c2a4816b",
+    "url": "/static/media/primeicons.4e915114.svg"
+  },
+  {
+    "revision": "79432094e4838b36d974b9d8c57d22e2",
+    "url": "/static/media/primeicons.79432094.woff"
+  }
+]);
