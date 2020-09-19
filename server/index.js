3200adefd4be85f033ac7a8247d765fc1a37d05b
Matt Hughes <zeepkdb@gmail.com>
2020-09-18 23:15:11 -0700

    dirname

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 const itemsRouter = require('../routes/items');
 app.use('/api/items', itemsRouter);
 app.get('*', (req, res) =>
-	res.sendFile(path.resolve('../client/build', 'index.html'))
+	res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
 );
 // app.get('*', function (response) {
 // 	response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
